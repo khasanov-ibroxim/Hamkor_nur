38938202ec3a771b951a547ad7bccf2b6bd73c53
ibrohim74 <xibroxim@gmail.com>
2024-11-27 21:11:27 +0500

    test

--- src/pages/production/production.jsx
+++ src/pages/production/production.jsx
@@ -11,6 +11,14 @@ import home_s5_2 from "../../assets/img/150A0765.webp";
 import home_s5 from "../../assets/img/150A1873.webp";
 import success from "../../assets/icon/success.png";
 
+
+import home_s3_1 from "../../assets/img/sigr.webp"
+import home_s3_2 from "../../assets/img/150A1861.webp"
+import home_s3_3 from "../../assets/img/150A1789.webp"
+import home_s3_4 from "../../assets/img/150A1786.webp"
+import home_s3_5 from "../../assets/img/150A1942.webp"
+import home_s3_6 from "../../assets/img/150A1873.webp"
+
 const Production = () => {
     const {id} = useParams();
     return (
@@ -18,7 +26,7 @@ const Production = () => {
             <div className="Home">
                 <span className={"opacity_container"}></span>
                 <div className="home_box container">
-                    <h1 data-aos="fade-up">производство</h1>
+                    <h1 data-aos="fade-up">Производство</h1>
                 </div>
                 <Swiper
                     modules={[Autoplay]}
@@ -83,21 +91,18 @@ const Production = () => {
                 <section className="home_section5 overflow-hidden production_s4_item">
                     <div className="row justify-content-center align-items-center">
                         <div className="col-lg-6" data-aos="fade-right">
-                            <img src={home_s5} alt=""/>
+                            <img src={home_s3_1} alt=""/>
                         </div>
                         <div className="col-lg-6" data-aos="fade-left">
                             <div className="home_section5_text">
                                 <div className="home_section5_text_title">
-                                    <h1>We create Handcrafted Accesories</h1>
+                                    <h1>ДОБЫЧА И ПОДГОТОВКА СЫРЬЯ</h1>
                                     <span></span>
                                 </div>
                                 <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt mollit anim id est laborum.
+                                    Мы используем — шкуры крупного рогатого скота. Сначала шкура подвергается очистке и
+                                    удалению ненужных тканей и загрязнений, что является первым шагом в получении
+                                    качественного материала для дальнейшей обработки.
                                 </p>
                             </div>
                         </div>
@@ -108,44 +113,89 @@ const Production = () => {
                         <div className="col-lg-6" data-aos="fade-left">
                             <div className="home_section5_text">
                                 <div className="home_section5_text_title">
-                                    <h1>We create Handcrafted Accesories</h1>
+                                    <h1>ДУБЛЕНИЕ</h1>
                                     <span></span>
                                 </div>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt mollit anim id est laborum.
-                                </p>
+                                <p>На этом этапе кожа обрабатывается с помощью различных химических веществ или
+                                    растительных экстрактов, чтобы сделать её устойчивой к воздействию влаги, плесени и
+                                    других факторов. </p>
                             </div>
                         </div>
                         <div className="col-lg-6" data-aos="fade-right">
-                            <img src={home_s5} alt=""/>
+                            <img src={home_s3_3} alt=""/>
                         </div>
                     </div>
                 </section>
                 <section className="home_section5 overflow-hidden production_s4_item">
                     <div className="row justify-content-center align-items-center">
                         <div className="col-lg-6" data-aos="fade-right">
-                            <img src={home_s5} alt=""/>
+                            <img src={home_s3_5} alt=""/>
                         </div>
                         <div className="col-lg-6" data-aos="fade-left">
                             <div className="home_section5_text">
                                 <div className="home_section5_text_title">
-                                    <h1>We create Handcrafted Accesories</h1>
+                                    <h1>МЕХАНИЧЕСКАЯ ОБРАБОТКА</h1>
                                     <span></span>
                                 </div>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt mollit anim id est laborum.
-                                </p>
+                                <p>После дубления кожа подвергается механической обработке, включая растяжку,
+                                    выравнивание и прессование. Эти процедуры помогают улучшить текстуру кожи, придавая
+                                    ей необходимую мягкость, эластичность и прочность.</p>
                             </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="home_section5 overflow-hidden production_s4_item">
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col-lg-6" data-aos="fade-left">
+                            <div className="home_section5_text">
+                                <div className="home_section5_text_title">
+                                    <h1>КРАСКА </h1>
+                                    <span></span>
+                                </div>
+                                <p>Для улучшения внешнего вида и долговечности кожи используются различные технологии
+                                    окрашивания. В зависимости от пожелания клиента.</p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6" data-aos="fade-right">
+                            <img src={home_s3_2} alt=""/>
+                        </div>
+                    </div>
+                </section>
+                <section className="home_section5 overflow-hidden production_s4_item">
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col-lg-6" data-aos="fade-right">
+                            <img src={home_s3_4} alt=""/>
+                        </div>
+                        <div className="col-lg-6" data-aos="fade-left">
+                            <div className="home_section5_text">
+                                <div className="home_section5_text_title">
+                                    <h1>ОБРАБОТКА И ОТДЕЛКА</h1>
+                                    <span></span>
+                                </div>
+                                <p>На этом этапе кожа проходит финишную обработку, которая может включать различные виды
+                                    отделки в зависимости от назначения и желаемых характеристик конечного продукта. Это
+                                    может быть тиснение, нанесение лакового покрытия, создание текстурных эффектов или
+                                    добавление защитных слоёв.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="home_section5 overflow-hidden production_s4_item">
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col-lg-6" data-aos="fade-left">
+                            <div className="home_section5_text">
+                                <div className="home_section5_text_title">
+                                    <h1>КОНТРОЛЬ КАЧЕСТВА</h1>
+                                    <span></span>
+                                </div>
+                                <p>Важной частью кожевенного производства является строгий контроль на каждом этапе. Мы
+                                    внимательно следим за качеством сырья, точностью выполнения всех технологических
+                                    операций и окончательной отделкой продукции, чтобы каждый клиент получил идеальный
+                                    продукт.</p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6" data-aos="fade-right">
+                            <img src={home_s3_6} alt=""/>
                         </div>
                     </div>
                 </section>
