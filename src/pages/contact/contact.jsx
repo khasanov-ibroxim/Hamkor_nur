4ad7c1b7b1d650d88a36ac281bb65470cba6c8a5
ibrohim74 <xibroxim@gmail.com>
2024-12-01 03:17:01 +0500

    test

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./contact.css";
 import contactFormImage from "../../assets/img/150A0695.webp";
-import { Helmet } from "react-helmet-async";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+import {Helmet} from "react-helmet-async";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Autoplay} from "swiper/modules";
 import contactHeader1 from "../../assets/img/150A0784.webp";
 import contactHeader2 from "../../assets/img/150A0785.webp";
 
@@ -16,6 +16,7 @@ const contactData = [
         telephone: "+998 70 201 00 23",
         index: "110300",
         address_url: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2885.0402375058584!2d69.652333!3d40.925115000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDU1JzMwLjQiTiA2OcKwMzknMDguNCJF!5e1!3m2!1sru!2s!4v1732637783498!5m2!1sru!2s",
+        map_url:"https://maps.google.com/maps?q=40.925115,69.652334&ll=40.925115,69.652334&z=16"
     },
     {
         id: 2,
@@ -26,6 +27,7 @@ const contactData = [
         telephone2: "+998 97 975 08 54",
         index: "110300",
         address_url: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2867.0368135724198!2d69.16743!3d41.335812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDIwJzA4LjkiTiA2OcKwMTAnMDIuOCJF!5e1!3m2!1sru!2s!4v1732721753219!5m2!1sru!2s",
+        map_url:"https://maps.google.com/maps?q=41.335813,69.167431&ll=41.335813,69.167431&z=16"
     },
     {
         id: 3,
@@ -35,6 +37,7 @@ const contactData = [
         telephone: "+998 97 273 10 03",
         index: "110300",
         address_url: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2890.6947855265726!2d72.332715!3d40.795421000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDQ3JzQzLjUiTiA3MsKwMTknNTcuOCJF!5e1!3m2!1sru!2s!4v1732773121570!5m2!1sru!2s",
+        map_url: "https://maps.google.com/maps?q=40.795422,72.332715&ll=40.795422,72.332715&z=16"
     },
 ];
 
@@ -59,7 +62,7 @@ const Contact = () => {
                     name="keywords"
                     content="контакт, форма связи, Ташкент, качественная продукция, обсуждение идей, кожа, кожевенное производство"
                 />
-                <meta name="robots" content="index, follow" />
+                <meta name="robots" content="index, follow"/>
             </Helmet>
 
             {/* Header Section */}
@@ -78,10 +81,10 @@ const Contact = () => {
                     className="mySwiper"
                 >
                     <SwiperSlide>
-                        <img src={contactHeader1} alt="Contact Header 1" />
+                        <img src={contactHeader1} alt="Contact Header 1"/>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={contactHeader2} alt="Contact Header 2" />
+                        <img src={contactHeader2} alt="Contact Header 2"/>
                     </SwiperSlide>
                 </Swiper>
             </section>
@@ -94,7 +97,7 @@ const Contact = () => {
                             <img
                                 src={contactFormImage}
                                 alt="Contact Form"
-                                style={{ objectFit: "cover" }}
+                                style={{objectFit: "cover"}}
                             />
                         </div>
                         <div className="col-lg-7">
@@ -106,9 +109,9 @@ const Contact = () => {
                                         — и мы обязательно найдем для вас решение.
                                     </p>
                                 </div>
-                                <input type="text" placeholder="Имя" required />
-                                <input type="email" placeholder="Электронная почта" required />
-                                <textarea rows="5" placeholder="Сообщение" />
+                                <input type="text" placeholder="Имя" required/>
+                                <input type="email" placeholder="Электронная почта" required/>
+                                <textarea rows="5" placeholder="Сообщение"/>
                                 <button type="submit">Отправить сообщение</button>
                             </form>
                         </div>
@@ -143,16 +146,18 @@ const Contact = () => {
                                 ))}
                             </select>
                             <h6 className="contact_address_address">
-                                <a href={selectedContact.address_url}>
+                                <a href={selectedContact.map_url}>
                                     {selectedContact.address_name} , {selectedContact.address}
                                 </a>
                             </h6>
                             <h6 className="contact_address_address" style={{
-                                lineHeight:"30px"
+                                lineHeight: "30px"
                             }}>
-                                Телефон: {selectedContact.telephone} <br/>
+                                Телефон: <a href={`tel:${selectedContact.telephone}`}>{selectedContact.telephone}</a>
+                                <br/>
                                 {selectedContact.telephone2 && (
-                                    <>Телефон: {selectedContact.telephone2}</>
+                                    <>Телефон: <a
+                                        href={`tel:${selectedContact.telephone2}`}>{selectedContact.telephone2}</a></>
                                 )}
                             </h6>
                             <h6 className="contact_address_address">
@@ -164,7 +169,7 @@ const Contact = () => {
                                 src={selectedContact.address_url}
                                 width="100%"
                                 height="300px"
-                                style={{ borderRadius: "20px" }}
+                                style={{borderRadius: "20px"}}
                                 allowFullScreen=""
                                 loading="lazy"
                                 referrerPolicy="no-referrer-when-downgrade"
